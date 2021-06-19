70b217fe6de497c1e41f66f56eea9ef7be068d9e
Naveen <naveen.ece.1728@iiitbh.ac.in>
2021-06-19 15:06:40 +0530

    Minor Updates on Produces

--- src/main/java/com/example/Assignment/HelloResource.java
+++ src/main/java/com/example/Assignment/HelloResource.java
@@ -2,23 +2,23 @@ package com.example.Assignment;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
-
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 @Path("/find-location")
 public class HelloResource {
     @POST
-    @Produces(MediaType.APPLICATION_JSON)
+    @Produces("application/json")
     public ArrayList<Location> getAddress(String jsonBody) throws IOException {
         ArrayList<Location> arrayList = new ArrayList<>();
+        JSONObject reader = new JSONObject(jsonBody);
+        Iterator<String> iteratorObj = reader.keys();
         JSONObject jsonObject = new JSONObject(jsonBody);
-        JSONArray add = jsonObject.getJSONArray("add");
+        JSONArray add = jsonObject.optJSONArray(iteratorObj.next());
         SendAPIRequest sendAPIRequest = new SendAPIRequest();
         for(int i = 0 ; i < add.length() ; i++){
             arrayList.add(sendAPIRequest.sendAPIRequest(add.getString(i)));
